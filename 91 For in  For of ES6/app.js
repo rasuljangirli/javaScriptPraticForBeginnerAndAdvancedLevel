8ad1330dba06aa99d024a6d959844952c565a42f
Rasul Jangirli <resulcahangirli99@gmail.com>
2024-06-27 22:39:05 +0400

    Create app.js

---
+++ 91 For in  For of ES6/app.js
@@ -0,0 +1,23 @@
+
+let names = ["Rasul", "Alisa", "Yusif", "Ceyda", "Elif",];
+
+
+// names.forEach((name , index) => console.log(name,index));
+
+
+// for(let name in names) {
+//     console.log(name)
+// }
+
+// for(let name in names) {
+//     console.log(name, names[name]);
+// }
+
+
+// for(name of names) {
+//     console.log(name);
+// }
+
+// for(name of names) {
+//     console.log(name , names.indexOf(name));
+// }
