081e01390f0069ed04e78423563edf2a7443a072
Rasul Jangirli <resulcahangirli99@gmail.com>
2024-06-30 23:57:41 +0400

    fff

---
+++ 99 this super super()   OOP/app.js
@@ -0,0 +1,49 @@
+
+class Person {
+    
+
+    constructor(firstName, lastName, salary) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    };
+
+
+    writeInfo() {
+        console.log(this.firstName, this.lastName, this.salary);
+    };
+};
+
+class Student extends Person {
+
+    constructor(firstName, lastName, salary) {
+        super(firstName, lastName, salary);
+    };
+
+    writeInfo() {
+        super.writeInfo();
+    }
+
+};
+
+class Enginer extends Person {
+
+    constructor(firstName, lastName, salary) {
+        super(lastName, firstName, salary);
+    }
+
+    writeInfo() {
+        super.writeInfo();
+    }
+}
+
+const person1 = new Person("Rasul", "jangirli", 30000);
+person1.writeInfo();
+
+const student1 = new Student("Arzu", "Ceferli", 20000);
+student1.writeInfo();
+
+const enginer1 = new Enginer("Mark", "Mask", 10000);
+enginer1.writeInfo();
+
