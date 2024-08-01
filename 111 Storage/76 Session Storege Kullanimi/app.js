fe57aa834cb95b8a9f6b9ec3b0145c94eb5a1d34
rasuljangirli <127443671+rasuljangirli@users.noreply.github.com>
2024-08-01 21:24:52 +0400

    Update app.js

--- 111 Storage/76 Session Storege Kullanimi/app.js
+++ 111 Storage/76 Session Storege Kullanimi/app.js
@@ -1,13 +1,11 @@
  //! Esasen yadda saxlamaliyiq ki biz her hansi bir bilgiyi Locak ve ya section storice elave edirikse orada string olaraq qebul edilir sonra onu get deyib cagirdiqimizda ise yene de string donur bunun ucun arreyleri gomduyumuz zaman json ile arrey olaraq kayd etmeliyiz ayni zamanda get etdiyimiz zamanda json olarak cagirmamiz gerek yoxsa array kimi yox sadece string kimi donmus olar.
- 
-
-//? Element Ekleme 
+  
 
 sessionStorage.setItem("1" , "Resul"); 
 sessionStorage.setItem("2" , "Ali"); 
 sessionStorage.setItem("3" , "Efe"); 
 
-//? Eelement Silme
+
 
 // sessionStorage.removeItem("2");
 
@@ -22,11 +20,8 @@ sessionStorage.setItem("3" , "Efe");
 // }
 
 
-//? Hepsini Silme
 
 // sessionStorage.clear();
-
-//? Arrey yazdirma alma donusdurme
 
 let names = ["Resul", "Ali","Nihan","Mehdi","Xedice","Ulduz","Medine"];
 sessionStorage.setItem("names",JSON.stringify(names));
