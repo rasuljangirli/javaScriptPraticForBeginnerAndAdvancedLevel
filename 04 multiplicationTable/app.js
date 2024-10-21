bfe0087af376c12fb42f08efbfa293f32ed06033
rasuljangirli <resulcahangirli99@gmail.com>
2024-10-21 19:41:42 +0400

    new project

---
+++ 04 multiplicationTable/app.js
@@ -0,0 +1,7 @@
+//! vurma cedveli (multiplication table)
+for (let i = 1; i <= 10; i++) {
+    console.log(" Vurma Cedveli--------- " + i + " ------- Multiplication Table");
+    for (let j = 1; j <= 10; j++) {
+        console.log(i + " x " + j + " = " + (i * j));
+    }
+}
